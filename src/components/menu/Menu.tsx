d9dadc30beeb2f39e59e474909e8ac52cc0745bc
Anton-013 <antonchuvashovoren@mail.ru>
2025-03-02 15:31:26 +0300

    add Header

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Menu = () => {
+    return (
+        <StyledMenu>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Services</a>
+                </li>
+                <li>
+                    <a href="">Portfolio</a>
+                </li>
+                <li>
+                    <a href="">Store</a>
+                </li>
+                <li>
+                    <a href="">Blog</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </StyledMenu>
+    );
+};
+
+const StyledMenu = styled.nav`
+    ul {
+        display: flex;
+        gap: 30px;
+    }
+`
