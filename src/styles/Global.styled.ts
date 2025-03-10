8fdc85e9f9aebd95d45147cc721709e145d42c1c
Anton-013 <antonchuvashovoren@mail.ru>
2025-03-10 12:07:00 +0300

    colors

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import { theme } from "./Theme";
 
 export const GlobalStyle = createGlobalStyle`
     *,
@@ -14,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
         font-family: "Source Sans 3", sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid sans", "Helvetica Neue", sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        color: ${theme.colors.font};
     }
 
     a {
---
+++ src/styles/Theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        primaryBg: "#ffffff",
+        secondatyBg: "#EBEBEB",
+        tertiaryBg: "#252525",
+        accent: "#FBA820",
+
+        font: "#ffffff",
+    }
+}
